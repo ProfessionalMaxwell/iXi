062efd315515e07306d6ecfc76644cb298d9b3a2
XW <xw@XWs-MacBook-Pro.local>
2016-12-23 17:41:35 -0500

    did a thing

---
+++ application/views/tongue.php
@@ -0,0 +1,26 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Welcome Too</title>
+</head>
+<body>
+
+<div>
+	<h1>Truth turns to lies</h1>
+
+	<div>
+		<p><b>And Says</b></p>
+		<p>"How did we wind up here?"</p>
+	</div>
+
+</div>
+
+<div>
+	<p>ANSWER THE QUESTION</p>
+</div>
+
+</body>
+</html>
